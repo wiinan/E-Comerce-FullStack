8be97ce0fcd4cbf1d89cdda0da602895ad84edaf
wiinan <wiinan2011@gmail.com>
2021-02-25 10:28:48 -0300

    All archives

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+import bcrypt from 'bcryptjs';
+const data =  {
+    users: [
+        {
+            name:'Admin',
+            email:'admin@email.com',
+            password:bcrypt.hashSync('123',8),
+            isAdmin: true
+        },
+        {
+            name:'test',
+            email:'test@email.com',
+            password:bcrypt.hashSync('1234',8),
+            isAdmin:false
+        }
+    ],
+    product:[
+        {
+            name:'Mi A3',
+            image: '../images/91AHzW462WL._AC_SX466_.jpg',
+            price:1200,
+            countInStock:10,
+            brand:'Xiaomi',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality'
+        },
+        {
+            name:'Redmi 8',
+            image: '../images/91AHzW462WL._AC_SX466_.jpg',
+            price:1250,
+            countInStock:10,
+            brand:'Xiaomi',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality'
+        },
+        {
+            name:'MiA',
+            image: '../images/91AHzW462WL._AC_SX466_.jpg',
+            price:1200,
+            countInStock:10,
+            brand:'Xiaomi',
+            rating:5.0,
+            numReviews:10,
+            description:'high quality'
+        },
+        {
+            name:'Mi A2',
+            image: '../images/91AHzW462WL._AC_SX466_.jpg',
+            price:1200,
+            countInStock:10,
+            brand:'Xiaomi',
+            rating:3.5,
+            numReviews:10,
+            description:'high quality'
+        },
+        {
+            name:'Redmi 9',
+            image: '../images/91AHzW462WL._AC_SX466_.jpg',
+            price:1200,
+            countInStock:10,
+            brand:'Xiaomi',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality'
+        },
+    ]
+};
+export default data;
---
+++ backend/utils.js
@@ -0,0 +1,5 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (user) => {
+    return jwt.sign({_id:user._id,nome:user.name,email:user.email,isAdmin:user.isAdmin},process.env.JWT_SECRET || 'somethingsecret',{expiresIn:'30d'});
+}
